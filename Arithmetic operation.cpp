5d60e14acbe060175d06a05a52cce99c414fafa6
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-20 23:08:06 +0300

    Add files via upload

---
+++ Arithmetic operation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double x,y;
+	
+	cout<<"Enter first number: ";
+	cin>>x;
+	cout<<"Enter second number: ";
+	cin>>y;
+	cout<<"When you Devide you got result of: "<<x/y<<endl<<endl;
+	cout<<"When you Multiply you got result of: "<<x*y<<endl<<endl;
+	cout<<"When you Add you got result of: "<<x+y<<endl<<endl;
+	cout<<"When you Substract you got result of: "<<x-y<<endl<<endl;
+	return 0;
+}
